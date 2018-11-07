3444c5a9bcb79704e8f9c01e6911fdeeb0395e17
hugo <collisionbrain@gmail.com>
2018-11-06 21:31:02 -0600

    reparingpromoter

---
+++ Promotor/app/src/main/java/com/libre/escuadronpromotor/ui/pojos/Delivery.java
@@ -0,0 +1,18 @@
+package com.libre.escuadronpromotor.ui.pojos;
+
+import java.io.Serializable;
+
+/**
+ * Created by hugo on 27/10/18.
+ */
+
+public class Delivery implements Serializable {
+
+    public String user_key;
+    public String user_name;
+    public String user_idb64;
+    public String delivery_date;
+    public String products;
+    public String total;
+
+}
